c8d580ca04cfba6c496121d163f0e1658f5e5e6e
Soopin Kim <spkim8901@gmail.com>
2024-07-29 10:44:12 -0700

    test commit5

--- WeatherApp/App/Api.swift
+++ WeatherApp/App/Api.swift
@@ -10,7 +10,7 @@ import Foundation
 class Api {
     static let shared = Api()
     private init(){}
-    
+    private let test = "test"
     private let apiInfo = ApiInfo()
     let seattle: String = "CurrentWeather"
     let vancouver: String = "CurrentWeatherVancouver"
